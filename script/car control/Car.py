a29a491f3156c0767bfdaace5570398a2669c886
master-cds-fpt <45336552+master-cds-fpt@users.noreply.github.com>
2018-11-26 21:24:39 +0700

    Add car class

---
+++ script/car control/Car.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+class Car:
+    def __init__(self,speed = 5,angleSteer = 0, yaw, cte,startPoint, centerPoint,):
+        self.speed = speed
+        self.angleSteer = angleSteer
+
+    def 
