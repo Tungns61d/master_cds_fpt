b813443ded283498311997ef78aa3cce8ef432e7
master-cds-fpt <45336552+master-cds-fpt@users.noreply.github.com>
2018-11-26 21:25:12 +0700

    Add point class

---
+++ script/lane detection/Point.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Point:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def get_point():
+
+    def set_point():
+
+    def draw():
+
+    def print_point():
+
+    
